e4b093bd6a5f514049232582825b3407dafb6e0d
James Hansen <James.Hansen@entrust.com>
2023-04-19 13:38:55 +0100

    HashMap of word frequencies, just to get started manipulating stuff in Rust.

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,34 @@
+use std::collections::HashMap;
+use std::vec::Vec;
+
+
 mod wordlist;
 
 fn main() {
 
-    let mut maxlen = 0;
+    let mut frequencies = HashMap::new();
 
-    for line in wordlist::WORDLIST.lines() {
-        let len = line.len();
-        if len > maxlen {
-            maxlen = len;
-            println!("maxlen: {maxlen}, \"{line}\"");
+    for word in wordlist::WORDLIST.lines() {
+        let index = word.len();
+
+        if ! frequencies.contains_key(&index) {
+            frequencies.insert(index, 1);
+        } else {
+            let optional_count = frequencies.get(&index);
+            match optional_count {
+                Some(old_value) => frequencies.insert(index, old_value + 1),
+                None => panic!("I need to get better at writing Rust."),
+            };
         }
+    }
+
+    let mut keys = Vec::from_iter(frequencies.keys());
+
+    keys.sort();
+
+
+    for key in keys {
+        println!("{0}: {1}", key, frequencies[key]);
+
     }
 }
