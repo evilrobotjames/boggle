592ae98842ae90572aae3fca0bc5de6f148a7ed7
James Hansen <James.Hansen@entrust.com>
2023-04-19 13:51:52 +0100

    Use entry(), and_modify() and or_insert().

--- src/main.rs
+++ src/main.rs
@@ -9,26 +9,14 @@ fn main() {
     let mut frequencies = HashMap::new();
 
     for word in wordlist::WORDLIST.lines() {
-        let index = word.len();
-
-        if ! frequencies.contains_key(&index) {
-            frequencies.insert(index, 1);
-        } else {
-            let optional_count = frequencies.get(&index);
-            match optional_count {
-                Some(old_value) => frequencies.insert(index, old_value + 1),
-                None => panic!("I need to get better at writing Rust."),
-            };
-        }
+        let key = word.len();
+        frequencies.entry(key).and_modify(|val| *val += 1).or_insert(1);
     }
 
     let mut keys = Vec::from_iter(frequencies.keys());
-
     keys.sort();
-
 
     for key in keys {
         println!("{0}: {1}", key, frequencies[key]);
-
     }
 }
