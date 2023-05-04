75c6a17b6aeecc4e2b25ccd37abab72fefbec5bf
James Hansen <James.Hansen@entrust.com>
2023-05-04 15:31:02 +0100

    Rename Node fields.

--- src/words/tree.rs
+++ src/words/tree.rs
@@ -3,24 +3,24 @@ use std::vec::Vec;
 
 #[derive(Debug)]
 pub struct Node {
-    pub word: bool, // Rename to "complete_word"?
-    pub next: HashMap<char, Node>, // Rename to "children"?
+    pub complete_word: bool,
+    pub children: HashMap<char, Node>,
 }
 
 impl Node {
     fn new() -> Node {
-        Node { word: false, next: HashMap::new() }
+        Node { complete_word: false, children: HashMap::new() }
     }
 
     fn insert_string(&mut self, string: &str) {
         match string.chars().next() {
             None => {
                 // The string is now empty.  Mark this node as a complete word.
-                self.word = true;
+                self.complete_word = true;
             },
             Some(c) => {
                 // Ensure next LetterNode exists for the first letter
-                let next = self.next.entry(c).or_insert(Node::new());
+                let next = self.children.entry(c).or_insert(Node::new());
                 // Recurse through the remaining string
                 next.insert_string(&string[1..]);
             }
@@ -28,13 +28,13 @@ impl Node {
     }
 
     fn to_words_recurse(&self, words: &mut Vec<String>, potential_word: &mut String) {
-        for letter_to_node in self.next.iter() {
+        for letter_to_node in self.children.iter() {
             let letter = letter_to_node.0;
             let node = letter_to_node.1;
 
             potential_word.push(*letter);
 
-            if node.word {
+            if node.complete_word {
                 // this is a complete word.  add current potential_word to words vector and continue.
                 let confirmed_word = String::from(potential_word.as_str());
                 // TODO: Do we keep this list sorted in the first place?  Or sort it all at the end?
@@ -59,9 +59,9 @@ impl Node {
     pub fn contains_word(&self, word: &str) -> bool {
 
         match word.chars().next() {
-            None => self.word, // 'word' is out of characters, return whether this is a complete word or not.
+            None => self.complete_word, // 'word' is out of characters, return whether this is a complete word or not.
             Some(c) => {
-                match self.next.get(&c) {
+                match self.children.get(&c) {
                     None => false, // No child node for this letter, so not a complete word
                     Some(child) => child.contains_word(&word[1..])
                 }
