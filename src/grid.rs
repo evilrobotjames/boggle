4d2024f96b0a04f4b197c83c7d1958821fe51480
James Hansen <James.Hansen@entrust.com>
2023-05-11 10:33:56 +0100

    Switch to Vec rather than fixed array.
    Also, split DICE data out to separate file.

--- src/grid.rs
+++ src/grid.rs
@@ -1,31 +1,14 @@
 use core::fmt;
-
 use rand::{Rng, seq::SliceRandom};
 
-const NUM_DICE: usize = 16;
-const NUM_SIDES: usize = 6;
-const NUM_CELLS: usize = 16;
+pub mod dice;
+
 const NUM_ROWS: usize = 4;
 const NUM_COLS: usize = 4;
+const LEN_ROW: usize = NUM_COLS;
+const LEN_COL: usize = NUM_ROWS;
 
-static DICE: [[&str; NUM_SIDES]; NUM_DICE] = [
-    ["a", "a", "e", "e", "g", "n"],
-    ["a", "b", "b", "j", "o", "o"],
-    ["a", "c", "h", "o", "p", "s"],
-    ["a", "f", "f", "k", "p", "s"],
-    ["a", "o", "o", "t", "t", "w"],
-    ["h", "l", "n", "n", "r", "z"],
-    ["c", "i", "m", "o", "t", "u"], 
-    ["d", "e", "i", "l", "r", "x"],
-    ["d", "e", "l", "r", "v", "y"],
-    ["d", "i", "s", "t", "t", "y"],
-    ["e", "e", "g", "h", "n", "w"],
-    ["e", "e", "i", "n", "s", "u"],
-    ["e", "h", "r", "t", "v", "w"],
-    ["e", "i", "o", "s", "s", "t"],
-    ["e", "l", "r", "t", "t", "y"],
-    ["h", "i", "m", "n", "qu", "u"],
-];
+const NUM_CELLS: usize = NUM_ROWS * NUM_COLS;
 
 #[derive(Debug)]
 pub struct Cell <'a> {
@@ -42,14 +25,21 @@ impl <'a> Cell<'a> {
     }
 }
 
+pub enum Direction {
+    North,
+    East,
+    South,
+    West,
+}
+
 #[derive(Debug)]
 pub struct Grid <'a> {
-    cells: [Cell<'a>; NUM_CELLS],
+    cells: Vec<Cell<'a>>,
 }
 
 impl <'a> Grid <'_> {
 
-    pub fn new() -> Grid <'a> {
+    pub fn new() -> Self {
     
         // Create a new grid, with some random stuff in cells.
 
@@ -60,30 +50,22 @@ impl <'a> Grid <'_> {
 
         for dice_index in dice_order {
             let mut rng = rand::thread_rng();
-            let side_index = rng.gen_range(0..NUM_SIDES);
+            let side_index = rng.gen_range(0..dice::NUM_SIDES);
 
-            cells.push(Cell::new(DICE[dice_index][side_index]));
+            cells.push(Cell::new(dice::DICE[dice_index][side_index]));
         }
 
         Grid {
-            cells: [
-                cells.pop().expect("This has enough values"),
-                cells.pop().expect("This has enough values"),
-                cells.pop().expect("This has enough values"),
-                cells.pop().expect("This has enough values"),
-                cells.pop().expect("This has enough values"),
-                cells.pop().expect("This has enough values"),
-                cells.pop().expect("This has enough values"),
-                cells.pop().expect("This has enough values"),
-                cells.pop().expect("This has enough values"),
-                cells.pop().expect("This has enough values"),
-                cells.pop().expect("This has enough values"),
-                cells.pop().expect("This has enough values"),
-                cells.pop().expect("This has enough values"),
-                cells.pop().expect("This has enough values"),
-                cells.pop().expect("This has enough values"),
-                cells.pop().expect("This has enough values"),
-            ]
+            cells: cells,
+        }
+    }
+
+    pub fn go(index: usize, direction: Direction) -> Option<usize> {
+        match direction {
+            Direction::North => if index < LEN_ROW { None } else { Some(index - LEN_ROW) },
+            Direction::East => if index % LEN_ROW == 0 { None } else { Some(index + 1) },
+            Direction::South => None,
+            Direction::West => None,
         }
     }
 }
@@ -91,12 +73,19 @@ impl <'a> Grid <'_> {
 impl fmt::Display for Grid<'_> {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
 
+        assert!(self.cells.len() == NUM_COLS * NUM_ROWS);
+
         let mut output = String::new();
+
         for row in 0..NUM_ROWS {
+
+            if row > 0 {
+                output.push('\n');
+            }
+
             for col in 0..NUM_COLS {
                 output.push_str(format!("{} ", self.cells[NUM_COLS * row + col].value).as_str());
-            }
-            output.push('\n');
+            }            
         }
 
         write!(f, "{}", output)
---
+++ src/grid/dice.rs
@@ -0,0 +1,21 @@
+pub const NUM_DICE: usize = 16;
+pub const NUM_SIDES: usize = 6;
+
+pub static DICE: [[&str; NUM_SIDES]; NUM_DICE] = [
+    ["a", "a", "e", "e", "g", "n"],
+    ["a", "b", "b", "j", "o", "o"],
+    ["a", "c", "h", "o", "p", "s"],
+    ["a", "f", "f", "k", "p", "s"],
+    ["a", "o", "o", "t", "t", "w"],
+    ["h", "l", "n", "n", "r", "z"],
+    ["c", "i", "m", "o", "t", "u"], 
+    ["d", "e", "i", "l", "r", "x"],
+    ["d", "e", "l", "r", "v", "y"],
+    ["d", "i", "s", "t", "t", "y"],
+    ["e", "e", "g", "h", "n", "w"],
+    ["e", "e", "i", "n", "s", "u"],
+    ["e", "h", "r", "t", "v", "w"],
+    ["e", "i", "o", "s", "s", "t"],
+    ["e", "l", "r", "t", "t", "y"],
+    ["h", "i", "m", "n", "qu", "u"],
+];
