8209125eb8b52181957aa5bfb225d8efdb355f77
James <evilrobotjames@gmail.com>
2023-05-23 22:45:27 +0000

    Add nicely formatted output of the results

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@
 #[macro_use]
 extern crate lazy_static;
 use clap::Parser;
+use game::grid::Grid;
 
 mod dictionary;
 mod game;
@@ -12,11 +13,43 @@ struct Cli {
     values: Vec<String>,
 }
 
+fn display_solution(grid: &mut Grid) {
+    
+    let mut words_printed = Vec::new();
+    let mut length = usize::MAX;
+
+    for word in grid.words_found() {
+
+        // Anything less than three isn't really interesting.
+        if word.len() <= 3 { 
+            println!();
+            return;
+        }
+
+        // Don't print words we've already printed.
+        if words_printed.contains(word) {
+            continue;
+        } else {
+            words_printed.push(word.to_string());
+        }
+
+        // Print a new heading, or the word.
+        if word.len() < length {
+            length = word.len();
+            print!("\n\n{length}: {word} ")
+        } else {
+            print!("{word} ");
+        }
+    }
+    println!("");
+}
+
 fn main() {
 
     let args = Cli::parse();
     let mut grid;
 
+    // If given some values, create a game from them.  If not, create a random game to solve.
     if args.values.len() > 0 {
         let mut values = Vec::new();
         for value in args.values {
@@ -30,8 +63,10 @@ fn main() {
     } else {
         grid = game::new_random();
     }
-    println!("{}", grid);
-    
+
+    print!("{}", grid);
+
     game::solve(&mut grid, dictionary::contains_word);
-    println!("{:?}", grid.words_found);
+    
+    display_solution(&mut grid);
 }
